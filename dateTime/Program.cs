6ce387a94aafdd273300bd68305e6bc9eeff985c
Doğukan <60947113+Dogukan18@users.noreply.github.com>
2021-10-14 19:39:45 +0300

    Add files via upload

---
+++ dateTime/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace dateTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Date and hour 
+
+            /* DateTime zaman;
+             zaman = DateTime.Now;
+             Console.WriteLine("Tarih ve Saat: {0}", zaman);
+             Console.Read(); */
+
+
+            //Year-Month-Day-DayOfWeek
+
+            /*   DateTime zaman;
+               zaman = DateTime.Now;
+               int ay = zaman.Month;
+               int gun = zaman.Day;
+               int yil = zaman.Year;
+               DayOfWeek haftaningunu = zaman.DayOfWeek;
+               Console.WriteLine("Yıl: {0}", yil);
+               Console.WriteLine("Ay: {0}", ay);
+               Console.WriteLine("Gün: {0}", gun);
+               Console.WriteLine("Haftanın Günü: {0}", haftaningunu);
+               Console.Read(); */
+
+
+            //Date-Time Functions
+
+          /*  DateTime zaman;
+            zaman = DateTime.Now;
+            Console.WriteLine("****Tarih Saat Fonksiyonları****");
+            Console.WriteLine();
+            Console.WriteLine("Tarih (Sayısal): {0:d}", zaman);
+            Console.WriteLine("Tarih (Yazı): {0:D}", zaman);
+            Console.WriteLine("Tarih-Gün-Saat (Yazı): {0:f}", zaman);
+            Console.WriteLine("Tarih-Gün-Saat-Saniye (Yazı): {0:F}", zaman);
+            Console.WriteLine("Tarih-Saat (Sayısal): {0:g}", zaman);
+            Console.WriteLine("Tarih-Saat-Saniye (Sayısal): {0:G}", zaman);
+            Console.WriteLine("Ay (Yazı): {0:m}", zaman);
+            Console.WriteLine("Uluslararası Tarih Formatı: {0:r}", zaman);
+            Console.WriteLine("Saat-Saniye: {0:T}", zaman);
+            Console.WriteLine("Saat: {0:t}", zaman);
+            Console.WriteLine("Ay-Yıl: {0:Y}", zaman);
+            Console.Read(); */
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
